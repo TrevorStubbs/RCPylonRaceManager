177b0443c072f0af9e08893356514fc327092852
Trevor Stubbs <stubbste@gmail.com>
2021-03-15 11:57:37 -0700

    RaceDay Service crated

---
+++ RCPylonRaceManagerWithSQLServer/RCPylonRaceManagerWithSQLServer/Model/DTOs/RaceDayDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RCPylonRaceManagerWithSQLServer.Model.DTOs
+{
+    public class RaceDayDTO
+    {
+        public int SeasonId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
